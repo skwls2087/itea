7dcc4a67d634b8dd61ac680c0d14b7743bddbb6f
najin <skwls2087@gmail.com>
2020-06-22 18:28:16 +0900

    마스터폴더정리

--- itea/src/main/java/com/itea/member/service/UserDaoInterface.java
+++
@@ -1,5 +0,0 @@
-package com.itea.member.service;
-
-public class UserDaoInterface {
-	String searchId(@Param("user_name")String user_name, @Param("user_phone")String user_phone); // 유저 ID 찾기
-}
--- itea/src/main/java/com/itea/member/service/UserInterface.java
+++
@@ -1,5 +0,0 @@
-package com.itea.member.service;
-
-public class UserInterface {
-	int searchPassword(String user_id, String user_email, String key); // 회원 임시 비밀번호 변경 메서드
-}
