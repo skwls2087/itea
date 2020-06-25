3e90dda8a3c5d170d3711c889cc30a6d59ac9379
najin <skwls2087@gmail.com>
2020-06-25 19:13:50 +0900

    다 추가

---
+++ itea/src/main/java/com/itea/member/controller/JoinController.java
@@ -0,0 +1,39 @@
+package com.itea.member.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.itea.member.dao.JoinDAO;
+import com.itea.member.dto.MemberDTO;
+
+@Controller
+public class JoinController {
+	
+	@Autowired
+	JoinDAO jDao;
+	
+	@RequestMapping("/member/joinFrm")
+	public void joinFrm() {
+		
+	}
+	
+	
+	@RequestMapping("/member/joinProc")
+	public void joinProc(MemberDTO mDto) {
+		System.out.println("joinProc 진입");
+		System.out.println("mDto"+mDto);
+		jDao.joinProc(mDto);
+		
+	}
+	@RequestMapping("member/joinProcSNS")
+	public String joinProcSNS(MemberDTO mDto) {
+		System.out.println("joinProcSNS 진입");
+		System.out.println("mDto"+mDto);
+		jDao.joinProcSNS(mDto);
+		
+		return "../../index";
+	}
+	
+	
+}
