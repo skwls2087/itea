0c9891b4574587cd9be5bfbf9f99777439303625
skwls2087 <63686155+skwls2087@users.noreply.github.com>
2020-08-31 23:55:09 +0900

    워드클라우드8월추가 & 관리자 인터셉터추가

--- itea/src/main/java/com/itea/util/LoginCheck.java
+++ itea/src/main/java/com/itea/util/LoginCheck.java
@@ -11,12 +11,11 @@ public class LoginCheck extends HandlerInterceptorAdapter{
 			throws Exception {
 		
 		HttpSession session = request.getSession();
-		String mno = (String)session.getAttribute("MNICK");
+		String mnick = (String)session.getAttribute("MNICK");
 		
-		if( mno==null || mno.length()==0 ) { //로그인이 안된 경우
+		if( !mnick.equals("관리자") ) { //관리자가 아닐 경우
 			System.out.println("권한부족으로 로그인폼으로 이동");
-			
-			response.sendRedirect("../member/loginFrm.co");
+			response.sendRedirect("/member/loginFrm.co");
 			return false;
 		}else {
 			return true;
